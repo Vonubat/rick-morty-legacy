a980d7966dd14ac90abdc69771d1181fb389c2f4
Tabunov Egor <Vonubat@gmail.com>
2022-10-14 10:59:25 +0300

    feat: add Badge, Table components for Modal

--- react-app/src/components/Modal.tsx
+++ react-app/src/components/Modal.tsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { IDataForModal } from 'types/models';
+import Badge from './UI/Badge';
+import Table from './UI/Table';
 
 type MyProps = Pick<IDataForModal, 'episodesModal' | 'locationModal' | 'nameModal'>;
 
 type MyState = Record<string, never>;
+
 export default class Modal extends Component<MyProps, MyState> {
   render(): JSX.Element {
+    const { name: nameofLocation, type, dimension } = this.props.locationModal;
     return (
       <div
         className="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
@@ -16,14 +20,14 @@ export default class Modal extends Component<MyProps, MyState> {
         role="dialog"
       >
         <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable relative w-auto pointer-events-none">
-          <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-            <div className="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-              <h5
-                className="text-xl font-medium leading-normal text-gray-800"
+          <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current overflow-x-hidden">
+            <div className="modal-header flex flex-shrink-0 items-center justify-between px-4 py-2 border-b border-gray-200 rounded-t-md">
+              <h2
+                className="text-3xl font-medium leading-normal text-gray-800"
                 id="modalCenteredScrollableLabel"
               >
                 {this.props.nameModal}
-              </h5>
+              </h2>
               <button
                 type="button"
                 className="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
@@ -31,14 +35,23 @@ export default class Modal extends Component<MyProps, MyState> {
                 aria-label="Close"
               ></button>
             </div>
-            <div className="modal-body relative p-4">
-              <p>{this.props.locationModal.name}</p>
-              <p>{this.props.locationModal.type}</p>
-              <p>{this.props.locationModal.dimension}</p>
-              <p>Just like that.</p>
-              {/* <p>{this.props.episodes[this.props.modalId]?.name || 'test-episodes'}</p>
-              <p>{this.props.locations[this.props.modalId]?.name || 'test-episodes'}</p> */}
-              <p>Just like that.</p>
+            <div className="modal-body relative py-2 px-4">
+              <h3 className="text-2xl text-blue-600 font-mono text-center">Location:</h3>
+              <h3 className="text-1xl text-gray-400 font-mono text-center mb-2">
+                [Name =&gt; Type =&gt; Dimension]
+              </h3>
+              <div className="flex flex-wrap gap-2 items-center justify-center mb-6">
+                <Badge option={nameofLocation} />
+                <Badge option={type} />
+                <Badge option={dimension} />
+              </div>
+              <h3 className="text-2xl text-blue-600 font-mono text-center">
+                List of episodes in which this character appeared:
+              </h3>
+              <Table
+                heading={['Name', 'Air date', ' Episode']}
+                episodesModal={this.props.episodesModal}
+              />
             </div>
           </div>
         </div>
---
+++ react-app/src/components/UI/Table.tsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { IDataForModal } from 'types/models';
+
+type MyProps = {
+  heading: string[];
+} & Pick<IDataForModal, 'episodesModal'>;
+
+type MyState = Record<string, never>;
+
+export default class Table extends Component<MyProps, MyState> {
+  render(): JSX.Element {
+    return (
+      <div className="flex flex-col">
+        <div className="py-2 inline-block min-w-full sm:px-2 lg:px-8">
+          <table className="min-w-full">
+            <thead className="bg-white border-b">
+              <tr>
+                <th scope="col" className="text-sm font-medium text-gray-900 px-2 py-2 text-left">
+                  #
+                </th>
+                {this.props.heading.map(
+                  (item: string): JSX.Element => (
+                    <th
+                      key={item}
+                      scope="col"
+                      className="text-sm font-medium text-gray-900 px-2 py-2 text-left"
+                    >
+                      {item}
+                    </th>
+                  )
+                )}
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.episodesModal.slice(1).map(
+                (
+                  item: {
+                    name: string;
+                    air_date: string;
+                    episode: string;
+                  },
+                  index: number
+                ): JSX.Element => (
+                  <tr key={index + 1} className="bg-gray-100 border-b">
+                    <td className="px-2 py-2 text-sm font-medium text-gray-900">{index + 1}</td>
+                    <td className="text-sm text-gray-900 font-light px-2 py-2">
+                      <a
+                        href={`https://rickandmorty.fandom.com/wiki/Special:Search?query=${item.name}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="underline decoration-transparent hover:decoration-inherit transition duration-300 ease-in-out"
+                      >
+                        {item.name}
+                      </a>
+                    </td>
+                    <td className="text-sm text-gray-900 font-light px-2 py-2">{item.air_date}</td>
+                    <td className="text-sm text-gray-900 font-light px-2 py-2">{item.episode}</td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
