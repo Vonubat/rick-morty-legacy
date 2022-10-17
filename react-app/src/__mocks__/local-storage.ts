9908a6e8764539c7976244dd388916fba7f822e3
Tabunov Egor <Vonubat@gmail.com>
2022-10-17 14:44:48 +0300

    refactor: split Tests.tsx in to modules

---
+++ react-app/src/__mocks__/local-storage.ts
@@ -0,0 +1,35 @@
+// Mock local storage
+
+const localStorageMock = (function () {
+  let store: { [index: string]: string } = {};
+
+  return {
+    getItem(key: string): string {
+      return store[key];
+    },
+
+    setItem(key: string, value: string): void {
+      store[key] = value;
+    },
+
+    clear() {
+      store = {};
+    },
+
+    removeItem(key: string): void {
+      delete store[key];
+    },
+
+    getAll(): {
+      [index: string]: string;
+    } {
+      return store;
+    },
+  };
+})();
+
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+
+export const setLocalStorage = (id: string, data: unknown): void => {
+  window.localStorage.setItem(id, JSON.stringify(data));
+};
--- react-app/src/setupTests.ts
+++ react-app/src/setupTests.ts
@@ -3,48 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-
-// Mock local storage
-
-const localStorageMock = (function () {
-  let store: { [index: string]: string } = {};
-
-  return {
-    getItem(key: string): string {
-      return store[key];
-    },
-
-    setItem(key: string, value: string): void {
-      store[key] = value;
-    },
-
-    clear() {
-      store = {};
-    },
-
-    removeItem(key: string): void {
-      delete store[key];
-    },
-
-    getAll(): {
-      [index: string]: string;
-    } {
-      return store;
-    },
-  };
-})();
-
-Object.defineProperty(window, 'localStorage', { value: localStorageMock });
-
-export const setLocalStorage = (id: string, data: unknown): void => {
-  window.localStorage.setItem(id, JSON.stringify(data));
-};
-
-// Mock URL.createObjectURL
-
-window.URL.createObjectURL = function (obj: Blob | MediaSource): string {
-  return `${obj}`;
-};
+import './__mocks__/create-object-url';
 
 // Change default JEST Timeout
 
