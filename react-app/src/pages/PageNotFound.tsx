e9c67bfca816435d91dd7dfa0e97a57112b0a593
Tabunov Egor <Vonubat@gmail.com>
2022-10-18 18:31:15 +0300

    feat: rewrite the PageNotFound to FC component

--- react-app/src/pages/PageNotFound.tsx
+++ react-app/src/pages/PageNotFound.tsx
@@ -1,34 +1,32 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default class PageNotFound extends Component {
-  html: HTMLHtmlElement = document.querySelector('html') as HTMLHtmlElement;
+export const PageNotFound: () => JSX.Element = (): JSX.Element => {
+  const html: HTMLHtmlElement = document.querySelector('html') as HTMLHtmlElement;
 
-  componentDidMount(): void {
-    this.html.style.overflow = 'hidden';
-  }
+  useEffect((): (() => void) => {
+    html.style.overflow = 'hidden';
 
-  componentWillUnmount(): void {
-    this.html.style.overflow = 'auto';
-  }
+    return (): void => {
+      html.style.overflow = 'auto';
+    };
+  }, [html.style]);
 
-  render(): JSX.Element {
-    return (
-      <div className="background-img">
-        <div className="space"></div>
-        <div className="wrapper">
-          <div className="img-wrapper">
-            <span>44</span>
-          </div>
-          <p>
-            The page you are trying to search has been <br />
-            moved to another universe.
-          </p>
-          <NavLink end to="/">
-            <button type="button">GET ME HOME</button>
-          </NavLink>
+  return (
+    <div className="background-img">
+      <div className="space"></div>
+      <div className="wrapper">
+        <div className="img-wrapper">
+          <span>44</span>
         </div>
+        <p>
+          The page you are trying to search has been <br />
+          moved to another universe.
+        </p>
+        <NavLink end to="/">
+          <button type="button">GET ME HOME</button>
+        </NavLink>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
