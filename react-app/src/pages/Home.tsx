77911681966e48fa6ad98bbed162fb33fe2f444d
Tabunov Egor <Vonubat@gmail.com>
2022-10-19 19:03:24 +0300

    feat: rewrite the Modal to FC component

--- react-app/src/pages/Home.tsx
+++ react-app/src/pages/Home.tsx
@@ -14,7 +14,7 @@ import React, { Component } from 'react';
 import Api from 'api/api';
 import { LoadIndicator } from 'components/UI/Indicators/Load';
 import { ErrorIndicator } from 'components/UI/Indicators/Error';
-import Modal from 'components/Modal';
+import { Modal } from 'components/Modal';
 import { EPISODES, LOCATIONS } from 'constants/constants';
 
 type MyProps = Record<string, never>;
