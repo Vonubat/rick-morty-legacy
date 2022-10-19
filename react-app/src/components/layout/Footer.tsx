3609edd621ca14492ff4ef7828ee5183f2a2ae64
Tabunov Egor <Vonubat@gmail.com>
2022-10-19 17:20:22 +0300

    feat: rewrite the Footer to FC component

--- react-app/src/components/layout/Footer.tsx
+++ react-app/src/components/layout/Footer.tsx
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import githubLogo from 'assets/GitHub-Mark-120px-plus.png';
 
-export default class Footer extends Component {
-  render(): JSX.Element {
-    return (
-      <footer className="flex justify-between items-center py-2.5 px-6 bg-gray-200">
-        <a
-          className="text-gray-600 underline decoration-transparent hover:decoration-inherit transition duration-300 ease-in-out"
-          href="https://rs.school/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          © 2022 The Rolling Scopes
-        </a>
+export const Footer: () => JSX.Element = (): JSX.Element => {
+  return (
+    <footer className="flex justify-between items-center py-2.5 px-6 bg-gray-200">
+      <a
+        className="text-gray-600 underline decoration-transparent hover:decoration-inherit transition duration-300 ease-in-out"
+        href="https://rs.school/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        © 2022 The Rolling Scopes
+      </a>
 
-        <a href="https://github.com/Vonubat" target="_blank" rel="noopener noreferrer">
-          <img src={githubLogo} alt="githubLogo" className="w-12 hover:animate-spin" />
-        </a>
-      </footer>
-    );
-  }
-}
+      <a href="https://github.com/Vonubat" target="_blank" rel="noopener noreferrer">
+        <img src={githubLogo} alt="githubLogo" className="w-12 hover:animate-spin" />
+      </a>
+    </footer>
+  );
+};
--- react-app/src/components/layout/Layout.tsx
+++ react-app/src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Outlet } from 'react-router-dom';
-import Footer from './Footer';
+import { Footer } from './Footer';
 import Navigation from './Navigation';
 
 export default class Layout extends Component {
