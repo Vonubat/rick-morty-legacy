3631ccdd33f614448b572ecf08b9b9eb16f32d0a
Tabunov Egor <Vonubat@gmail.com>
2022-10-19 19:31:39 +0300

    feat: rewrite the SearchBar to FC component

--- react-app/src/pages/Home.tsx
+++ react-app/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { Card } from 'components/Card';
-import SearchBar from 'components/SearchBar';
+import { SearchBar } from 'components/SearchBar';
 import {
   ICharacter,
   ICharacterContent,
