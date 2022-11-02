43f6b9e4f2e0dcecf4faa938f99fcadabdd2c4c7
Tabunov Egor <Vonubat@gmail.com>
2022-11-02 03:55:40 +0300

    feat: add scroll-to-top utility

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -4,11 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/index.scss';
 import 'tw-elements';
+import ScrollToTop from 'utils/scroll-to-top';
 
 const root: Root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
